c5ae64330eb62148a4af82074001e40e3d4b9025
rajukvats <rajukvats@gmail.com>
2022-07-19 15:27:00 +0530

    validate

--- routes/appointmentRoute.js
+++ routes/appointmentRoute.js
@@ -17,7 +17,7 @@ router.post('/changeStatus', authHandler.auth_func, appointmentApis.changeStatus
 
 // Client Side
 router.post('/customerBooking', authHandler.auth_func, appointmentValidator.addByCustomer, appointmentApis.customerBooking);
-router.post('/bookingAppointmentSomeOneElse', authHandler.auth_func, appointmentValidator.addByCustomer, appointmentApis.bookingAppointmentSomeOneElse);
+// router.post('/bookingAppointmentSomeOneElse', authHandler.auth_func, appointmentValidator.addByCustomer, appointmentApis.bookingAppointmentSomeOneElse);
 router.post('/followUpBooking', authHandler.auth_func, appointmentValidator.addByCustomer, appointmentApis.followUpBooking);
 router.post('/getAppointmentDetail', authHandler.auth_func,appointmentApis.getAppointmentDetail);
 module.exports = router;
